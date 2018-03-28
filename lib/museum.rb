971f91ad621810027e3709537b25ccd10c2eaf14
justinetroyke <jjtroyke@gmail.com>
2018-03-28 11:01:33 -0600

    got to iteration 3 but needed to re-work how I was storing the patrons_of variable so it works for multiple patrons

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,14 +1,35 @@
 class Museum
 
   attr_reader :name,
-              :exhibits
+              :exhibits,
+              :revenue,
+              :visits
 
   def initialize(name)
     @name = name
     @exhibits = {}
+    @revenue = 0
+    @general_admittance = 10
+    @visits = {}
   end
 
   def add_exhibit(exhibit_name, cost)
     @exhibits.store(exhibit_name, cost)
+  end
+
+  def admit(patron)
+    @revenue += @general_admittance
+    list = patron.interests
+    patron_cost = []
+    list.map do |interest|
+      @visits.store(interest, patron.name)
+      cost = @exhibits[interest]
+      patron_cost << cost
+    end
+    @revenue += patron_cost.join.to_i
+  end
+
+  def patrons_of(exhibit)
+    @visits.values_at(exhibit)
   end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,4 +1,5 @@
 require './lib/museum'
+require './lib/patron'
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
@@ -28,5 +29,91 @@ class MuseumTest < Minitest::Test
     assert_equal ({"Dead Sea Scrolls" => 10, "Gems and Minerals" => 0}), dmns.exhibits
     assert_equal ["Dead Sea Scrolls","Gems and Minerals"], dmns.exhibits.keys
     assert_equal [10,0], dmns.exhibits.values
+  end
+
+  def test_museum_has_revenue_and_it_is_0
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_equal 0, dmns.revenue
+  end
+
+  def test_museum_can_admit_patrons_with_general_admittance
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    bob = Patron.new("Bob")
+    bob.add_interest("Gems and Minerals")
+    dmns.admit(bob)
+
+    assert_equal 10, dmns.revenue
+  end
+
+  def test_when_partron_interest_and_exhibit_are_same_revenue_increases
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    dmns.add_exhibit("Dead Sea Scrolls", 10)
+    dmns.add_exhibit("Gems and Minerals", 0)
+    bob = Patron.new("Bob")
+    bob.add_interest("Dead Sea Scrolls")
+    dmns.admit(bob)
+
+    assert_equal 20, dmns.revenue
+  end
+
+  def test_patron_can_have_more_than_one_interest
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    dmns.add_exhibit("Dead Sea Scrolls", 10)
+    dmns.add_exhibit("Gems and Minerals", 0)
+    bob = Patron.new("Bob")
+    bob.add_interest("Gems and Minerals")
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Imax")
+    dmns.admit(bob)
+
+    assert_equal 20, dmns.revenue
+  end
+
+  def test_multiple_patrons_can_have_more_than_one_interest
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    dmns.add_exhibit("Dead Sea Scrolls", 10)
+    dmns.add_exhibit("Gems and Minerals", 0)
+    bob = Patron.new("Bob")
+    bob.add_interest("Gems and Minerals")
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Imax")
+    dmns.admit(bob)
+    sally = Patron.new("Sally")
+    sally.add_interest("Dead Sea Scrolls")
+    dmns.admit(sally)
+
+    assert_equal 40, dmns.revenue
+  end
+
+  def test_it_can_list_one_patron_that_visit_exhibit
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    dmns.add_exhibit("Dead Sea Scrolls", 10)
+    dmns.add_exhibit("Gems and Minerals", 0)
+    bob = Patron.new("Bob")
+    bob.add_interest("Dead Sea Scrolls")
+    dmns.admit(bob)
+
+    assert_equal ["Bob"], dmns.patrons_of("Dead Sea Scrolls")
+    assert_equal [nil], dmns.patrons_of("Gems and Minerals")
+  end
+
+  def test_it_can_list_patrons_that_visited_exhibit
+    skip
+    # I know I would have needed to re-work my code order since the location of
+    # would cause the value of @visits.store(interest, patron.name) to reset each time it went
+    # through the enum.
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    dmns.add_exhibit("Dead Sea Scrolls", 10)
+    dmns.add_exhibit("Gems and Minerals", 0)
+    bob = Patron.new("Bob")
+    bob.add_interest("Dead Sea Scrolls")
+    dmns.admit(bob)
+    sally = Patron.new("Sally")
+    sally.add_interest("Dead Sea Scrolls")
+    dmns.admit(sally)
+
+    assert_equal ["Bob", "Sally"], dmns.patrons_of("Dead Sea Scrolls")
+    assert_equal [nil], dmns.patrons_of("Gems and Minerals")
   end
 end
